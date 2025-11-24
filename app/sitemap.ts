f2db28d8f4fd2cb2ae7911832e0f79d78c002783
Edun Yusuf <yusufedun19@gmail.com>
2025-11-24 01:57:14 +0100

    new

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,7 @@ import type { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ""
 
+  
   return [
     {
       url: baseUrl,
